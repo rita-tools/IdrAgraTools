ab89b512c50241ac6fe0e3b29bedb2dda3b5bc0f
echiaradia <enrico.chiaradia@unimi.it>
2022-11-24 07:38:02 +0100

    add empty parameters file to fill not setted method ids

--- tools/export_irrigation_method.py
+++ tools/export_irrigation_method.py
@@ -34,73 +34,99 @@ from .speakingName import speakingName
 from .write_pars_to_template import writeParsToTemplate
 
 def exportIrrigationMethod(DBM,outPath, feedback = None,tr=None):
-	irrIdList = DBM.getUniqueValues(fieldName = 'id', tableName= 'idr_irrmet_types')
+	maxIrrMethId = DBM.getMax('idr_irrmet_types', 'id')
+	if maxIrrMethId is None:
+		feedback.reportError('No irrigation method parameters to be processed. Exiting...', True)
+
 	irrRecs = []
 		
-	for irrId in irrIdList:
-		# create crop params folder
-		irrMethod = DBM.getRecord(tableName = 'idr_irrmet_types',fieldsList='',filterFld='id', filterValue=irrId)[0]
-		irrMethod = irrMethod[1:] #remove first field "fid"
-		feedback.pushInfo(tr('Exporting settings for irrigation method %s - %s') % (irrMethod[0], irrMethod[1]))
-		# prepare new file name
-		fileName = '%s_%s.txt'%(irrId,speakingName(irrMethod[1]))
+	for irrId in range(1, maxIrrMethId + 1):
+		# make default parameters with empty values
+		irrDict = {'ID': 0,
+				   'NAME': 'not implemented',
+				   'QADAQ': 0,
+				   'KSTRESS': 0,
+				   'KSTRESSWELL': 0,
+				   'FW': 0,
+				   'AMIN': 0,
+				   'AMAX': 0,
+				   'BMIN': 0,
+				   'BMAX': 0,
+				   'ALOSSES': 0,
+				   'BLOSSES': 0,
+				   'CLOSSES': 0,
+				   'FINTERCEPTION': 0,
+				   'IRRTIMETABLE': '',
+				   'ADV_OPTS': ''
+				   }
 
-		table = []
-		flowRates = irrMethod[15].split(' ')
-		starList = []
-		totFract = 0.0
 
-		for i,fr in enumerate(flowRates):
-			if fr =='*':
-				starList.append(i)
-			else:
-				totFract += float(fr)
+		# load irrigation params from db
+		irrMethod = DBM.getRecord(tableName = 'idr_irrmet_types',fieldsList='',filterFld='id', filterValue=irrId)
+		if len(irrMethod) > 0:
+			irrMethod = irrMethod[0]
+			irrMethod = irrMethod[1:] #remove first field "fid"
+			feedback.pushInfo(tr('Exporting settings for irrigation method %s - %s') % (irrMethod[0], irrMethod[1]))
 
-		numOfStar = len(starList)
-		if totFract>1.0:
-			feedback.reportError(tr('Error in irrigation run time, total ratio exceed 1.0'), False)
+			table = []
+			flowRates = irrMethod[15].split(' ')
+			starList = []
+			totFract = 0.0
 
-		if ((totFract<1.0) and (numOfStar>0)):
-			# equally distribute difference to missing values
-			for i in starList:
-				flowRates[i] = str((1.0-totFract)/numOfStar)
+			for i,fr in enumerate(flowRates):
+				if fr =='*':
+					starList.append(i)
+				else:
+					totFract += float(fr)
 
-		aZip = zip(irrMethod[14].split(' '),flowRates)
-		for z in aZip:
-			table.append('%s = %s # Irrigation between %s:00 and %s:59'%(z[0],z[1],str(int(z[0])-1).zfill(2),str(int(z[0])-1).zfill(2)))
-			
-		table = '\n'.join(table)
-		
-		f_int = 'T'
-		if irrMethod[13] in ['0',0,'F','FALSE','false','False']:
-			f_int = 'F'
+			numOfStar = len(starList)
+			if totFract>1.0:
+				feedback.reportError(tr('Error in irrigation run time, total ratio exceed 1.0'), False)
 
-		adv_opts = ''
-		if len(irrMethod) > 17:  # for legacy db structure
-			adv_opts = irrMethod[17].split(';')
-			adv_opts = [opt.strip() for opt in adv_opts]
-			adv_opts = '\n'.join(adv_opts)
+			if ((totFract<1.0) and (numOfStar>0)):
+				# equally distribute difference to missing values
+				for i in starList:
+					flowRates[i] = str((1.0-totFract)/numOfStar)
 
-		irrDict = {'ID':irrMethod[0],
-						'NAME':irrMethod[1],
-						'QADAQ':irrMethod[2],
-						'KSTRESS':irrMethod[3],
-						'KSTRESSWELL':irrMethod[4],
-				   		'FW':irrMethod[5],
-						'AMIN':irrMethod[6],
-						'AMAX':irrMethod[7],
-						'BMIN':irrMethod[8],
-						'BMAX':irrMethod[9],
-						'ALOSSES':irrMethod[10],
-						'BLOSSES':irrMethod[11],
-						'CLOSSES':irrMethod[12],
-						'FINTERCEPTION':f_int,
-						'IRRTIMETABLE':table,
-				   		'ADV_OPTS': adv_opts
-				}
-		
+			aZip = zip(irrMethod[14].split(' '),flowRates)
+			for z in aZip:
+				table.append('%s = %s # Irrigation between %s:00 and %s:59'%(z[0],z[1],str(int(z[0])-1).zfill(2),str(int(z[0])-1).zfill(2)))
+
+			table = '\n'.join(table)
+
+			f_int = 'T'
+			if irrMethod[13] in ['0',0,'F','FALSE','false','False']:
+				f_int = 'F'
+
+			adv_opts = ''
+			if len(irrMethod) > 17:  # for legacy db structure
+				adv_opts = irrMethod[17].split(';')
+				adv_opts = [opt.strip() for opt in adv_opts]
+				adv_opts = '\n'.join(adv_opts)
+
+			# replace default non implemented
+			irrDict = {'ID':irrMethod[0],
+							'NAME':irrMethod[1],
+							'QADAQ':irrMethod[2],
+							'KSTRESS':irrMethod[3],
+							'KSTRESSWELL':irrMethod[4],
+							'FW':irrMethod[5],
+							'AMIN':irrMethod[6],
+							'AMAX':irrMethod[7],
+							'BMIN':irrMethod[8],
+							'BMAX':irrMethod[9],
+							'ALOSSES':irrMethod[10],
+							'BLOSSES':irrMethod[11],
+							'CLOSSES':irrMethod[12],
+							'FINTERCEPTION':f_int,
+							'IRRTIMETABLE':table,
+							'ADV_OPTS': adv_opts
+					}
+
 		#loop in used crop and export
 		# save to file
+		fileName = '%s_%s.txt' % (irrDict['ID'], speakingName(irrDict['NAME']))
+
 		writeParsToTemplate(outfile=os.path.join(outPath,fileName),
 									parsDict =  irrDict,
 									templateName='irrigation_par.txt')
